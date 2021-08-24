89b2a168251f4f9190ff57c9a368395aac2ff12f
zqyz-ai <74638311+zqyz-ai@users.noreply.github.com>
2021-08-24 10:15:11 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,6 +14,6 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
 #默认主题
-rm -rf 包/精益/luci-theme-argon
+rm -rf package/lean/luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
-sed -i ' s/luci-theme-bootstrap/luci-theme-argon/ ' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
