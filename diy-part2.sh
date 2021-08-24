3e7a155d4716a1b48aac2978217cd5d0d3517d3a
zqyz-ai <74638311+zqyz-ai@users.noreply.github.com>
2021-08-24 10:11:39 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,8 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+#默认主题
+rm -rf 包/精益/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+sed -i ' s/luci-theme-bootstrap/luci-theme-argon/ ' feeds/luci/collections/luci/Makefile
